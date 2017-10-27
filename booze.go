0b4e541d7e524d378011d79ed5a82c1d05daa676
Jonathan Face <jonathan.fac@gmail.com>
2017-10-26 22:46:48 -0400

    basic UI and one area

--- booze.go
+++ booze.go
@@ -2,9 +2,14 @@ package main
 
 import (
   "net/http"
+  "strconv"
+  "encoding/json"
+  "log"
+  "fmt"
+  _ "github.com/go-sql-driver/mysql"
   "github.com/gorilla/mux"
   "github.com/gorilla/context"
-  "log"
+  "github.com/jonathanface/bumquest/DBUtils"
 )
 
 const (
@@ -12,8 +17,59 @@ const (
   PORT = ":8080"
 )
 
+type ObjectInfo struct {
+  Oid int `json:"oid"`
+  Title string `json:"title"`
+  Description string `json:"description"`
+}
+func forbidden (w http.ResponseWriter, err string) {
+  http.Error(w, err, http.StatusForbidden)
+}
+
+func serverError (w http.ResponseWriter, err string) {
+  http.Error(w, err, http.StatusInternalServerError)
+}
+
+func fileNotFound (w http.ResponseWriter, err string) {
+  http.Error(w, err, http.StatusNotFound)
+}
+
+func badRequest (w http.ResponseWriter, err string) {
+  http.Error(w, err, http.StatusBadRequest)
+}
+
+func writeSuccess(w http.ResponseWriter) {
+  w.WriteHeader(http.StatusOK)
+  w.Write([]byte("200 - Success"))
+}
+
+func handleObjectInfo(w http.ResponseWriter, r *http.Request) {
+  log.Println("get object info")
+  oid, err := strconv.Atoi(mux.Vars(r)["[0-9]+"])
+  if (err != nil) {
+    log.Fatal(err)
+    badRequest(w, err.Error())
+    return
+  }
+  var info = ObjectInfo{}
+  db := DBUtils.OpenDB();
+  db.QueryRow("select objectID,name,description from objects WHERE objectID = ?", oid).Scan(&info.Oid, &info.Title, &info.Description)
+  DBUtils.CloseDB(db)
+  if len(info.Description) == 0 {
+    fileNotFound(w, "No description")
+    return
+  }
+  jsonData, err := json.Marshal(info)
+  if (err != nil) {
+    serverError(w, err.Error())
+    return
+  }
+  fmt.Fprintf(w, string(jsonData))
+}
+
 func main() {
   rtr := mux.NewRouter()
+  rtr.HandleFunc(SERVICE_PATH + "/object/{[0-9]+}/look", handleObjectInfo).Methods("GET")
   rtr.PathPrefix("/").Handler(http.FileServer(http.Dir("./static/")))
   http.Handle("/", rtr)
   
--- static/js/main.js
+++ static/js/main.js
@@ -1,4 +1,77 @@
 
+const TEMPLATE_URL = 'templates/';
+const SERVICE_URL = 'service/';
+
 $(document).ready(function() {
-  console.log('woo hoo');
+  $('area').each(function(index, item) {
+    $(item).click(function(event) {
+      event.preventDefault();
+      event.stopPropagation();
+      showMenu(this, event.pageX - $('main').offset().left, event.pageY - $('main').offset().top);
+    });
+  });
 });
+
+function assignMenuTimer(menu) {
+  setTimeout(function() {
+    if ($(menu).find('.ui_icons:hover').length) {
+      assignMenuTimer(menu);
+    } else {
+      removeUIMenu(menu);
+    }
+  }, 3000);
+}
+
+function removeUIMenu(menu) {
+  $(menu).fadeOut('fast', function() {
+    $(this).remove();
+  });
+}
+
+function removeAllUIMenus() {
+  $('.ui_menu').fadeOut('fast', function() {
+    $(this).remove();
+  });
+}
+
+function lookAtObject(objectID) {
+  console.log('looking at...');
+  $.getJSON(SERVICE_URL + 'object/' + objectID + '/look', function(data) {
+    console.log(data);
+  });
+}
+
+function interact(action, object) {
+  console.log(isNaN(parseInt(object)));
+  console.log(object.length);
+  if (isNaN(parseInt(object))) {
+    console.log('??');
+    return;
+  }
+  switch(action) {
+    case 'look':
+      lookAtObject(object);
+      break;
+  }
+}
+
+function showMenu(object, xPos, yPos) {
+  removeAllUIMenus();
+  $.get(TEMPLATE_URL + 'interact_menu.html', function(template) {
+    template = $(template);
+    var left = xPos + 10;
+    var top = yPos - $(template).height()/2;
+    $(template).css('top', top );
+    $(template).css('left', left);
+    $('main').append(template);
+    $(template).find('.ui_icons img').each(function(index, item) {
+      $(item).click(function(event) {
+        interact($(item).data('action'), $(object).data('objid'));
+      });
+    });
+    $(template).fadeTo('fast', 1, function() {
+      assignMenuTimer(template);
+    });
+  });
+}
+
